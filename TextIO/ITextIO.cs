aaa8f7d2f9c79e78038af59abd9874932b5f0c0e
Stoffer123 <christophermikkelsen@live.dk>
2026-01-06 13:18:27 +0100

    Add project files.

---
+++ TextIO/ITextIO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TextIO
+{
+    public interface ITextIO
+    {
+        void put(string text);
+        string get();
+        int getInt();
+    }
+}
