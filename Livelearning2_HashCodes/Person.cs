4c663a1a75cee38ed2359ae8cec8a96a8e95b6ae
Stoffer123 <christophermikkelsen@live.dk>
2026-01-29 21:20:40 +0100

    Livelearning2

---
+++ Livelearning2_HashCodes/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Livelearning2_HashCodes
+{
+    public class Person
+    {
+        public string Fornavn { get; set; }
+        public string Efternavn { get; set; }
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return this.Id == ((Person)obj).Id;
+        }
+    }
+}
---
+++ Livelearning2_HashCodes/Program.cs
@@ -0,0 +1,28 @@
+﻿using Livelearning2_HashCodes;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var person1 = new Person { Fornavn = "John", Efternavn = "Doe" };
+        var person2 = new Person { Fornavn = "John", Efternavn = "Doe" };
+
+        var søgePerson = new Person { Fornavn = "", Efternavn = "", Id = person1.Id };
+
+
+        HashSet<Person> peopleSet = new HashSet<Person>();
+        Dictionary<Person, string> peopleDict = new Dictionary<Person, string>();
+
+        peopleSet.Add(person1);
+        var result = peopleSet.Contains(person2);
+
+        person1.Efternavn = "Smith";
+
+        result = peopleSet.Contains(søgePerson);
+
+
+        Console.WriteLine($"Person 1 HashCode: {person1.GetHashCode()}");
+        Console.WriteLine($"Person 2 HashCode: {person2.GetHashCode()}");
+        Console.WriteLine($"Are they equal? {person1.Equals(person2)}");
+    }
+}
