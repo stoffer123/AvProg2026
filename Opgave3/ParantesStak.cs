bc074ab49b1f9e9bc6e1f53d0cd1e7c40e1d8ecb
Stoffer123 <christophermikkelsen@live.dk>
2026-02-03 17:39:07 +0100

    3.1 og 3.2

---
+++ Opgave3/ParantesStak.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opgave3
+{
+    public static class ParantesStak
+    {
+        public static bool IsBalancedStack(string input)
+        {
+            Stack<char> stack = new Stack<char>();
+
+            Dictionary<char, char> matchingPairs = new()
+            {
+                {')', '(' },
+                {']', '[' },
+                {'}', '{' }
+            };
+
+            foreach(char c in input)
+            {
+                if(c == '(' || c == '[' || c == '{')
+                {
+                    stack.Push(c);
+                }
+                else if(c == ')' || c == ']' || c == '}')
+                {
+                    if(stack.Count == 0)
+                    {
+                        return false;
+                    }
+
+                    char top = stack.Pop();
+                    if(top != matchingPairs[c])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return stack.Count == 0;
+
+        }
+    }
+}
--- Opgave3/StringOperations.cs
+++ Opgave3/StringOperations.cs
@@ -2,11 +2,86 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Opgave3
 {
-    internal class StringOperations
+    public static class StringOperations
     {
+        public static string Cut(this string s, int n)
+        {
+            if (s.Length > n)
+            {
+                return s.Substring(0, n);
+            }
+
+            return s;
+
+        }
+
+        public static string LeftAdjust(this string s, int n, char c)
+        {
+            if (s.Length >= n)
+            {
+                return s.PadRight(n, c);
+            }
+            return s;
+        }
+
+        public static string RightAdjust(this string s, int n, char c)
+        {
+            if (s.Length >= n)
+            {
+                return s.PadLeft(n, c);
+            }
+            return s;
+        }
+
+        public static string CenterAdjust(this string s, int n, char c)
+        {
+            if(s.Length >= n)
+            {
+                return s;
+            }
+
+            int totalPadding = n - s.Length;
+            int padLeft = totalPadding / 2;
+            int padRight = totalPadding - padLeft;
+
+            s = s.PadLeft(s.Length + padLeft, c);
+            s = s.PadRight(s.Length + padRight, c);
+            return s;
+        }
+
+        public static string RemoveChars(this string s, char c)
+        {
+            return string.Concat(s.Where(ch => ch != c)).ToString();
+        }
+
+        public static List<string> Split(this string s, string[] seperators, bool includeEmpty, bool includeSeperators)
+        {
+            string pattern = string.Join("|", seperators.Select(sep => Regex.Escape(sep)));
+
+            string[] parts;
+            if (includeSeperators)
+            {
+                parts = Regex.Split(s, $"({pattern})");
+            }
+            else
+            {
+                parts = Regex.Split(s, pattern);
+
+            }
+
+            if (includeEmpty)
+            {
+                return parts.ToList();
+            }
+            else
+            {
+                return parts.Where(p => p.Length > 0).ToList();
+            }
+        }
     }
 }
