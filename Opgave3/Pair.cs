9da0aa57f05fa1f9340638ca6a9160e56e143567
Stoffer123 <christophermikkelsen@live.dk>
2026-02-03 20:47:47 +0100

    Opgave 3.3

---
+++ Opgave3/Pair.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opgave3
+{
+    public class Pair<T1, T2> : IComparable<Pair<T1, T2>>
+        where T1 : IComparable<T1>
+        where T2 : IComparable<T2>
+    {
+        public T1 Arg1 { get; set; }
+        public T2 Arg2 { get; set; }
+
+        public Pair(T1 arg1, T2 arg2)
+        {
+            Arg1 = arg1;
+            Arg2 = arg2;
+        }
+
+        public void Clear()
+        {
+            Arg1 = default(T1);
+            Arg2 = default(T2);
+        }
+
+        public int CompareTo(Pair<T1, T2>? other)
+        {
+            var result = Arg1.CompareTo(other.Arg1);
+            if (result == 0)
+            {
+                result = Arg2.CompareTo(other.Arg2);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Opgave3/PairList.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opgave3
+{
+    public class PairList<T1, T2>
+        where T1 : Pair<T1, T2>
+        where T2 : Pair<T1, T2>
+    {
+        private List<Pair<T1, T2>> _list = new();
+
+        public void Add(Pair<T1, T2> pair)
+        {
+            _list.Add(pair);
+        }
+
+        public void Sort()
+        {
+            _list.Sort();
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var pair in _list)
+            {
+                sb.AppendLine($"({pair.Arg1}, {pair.Arg2})");
+            }
+            return sb.ToString();
+        }
+
+    }
+}
